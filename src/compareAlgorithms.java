47777ec66f74eaefb87e100bcc1dc438d18e53c1
Sujith Shivaprakash <sujishiv@iu.edu>
2016-10-09 02:00:40 -0400

    Removed unused imports and added short queries.

--- src/compareAlgorithms.java
+++ src/compareAlgorithms.java
@@ -1,50 +1,26 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
-
 import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.search.similarities.ClassicSimilarity;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.LeafReaderContext;
-import org.apache.lucene.index.MultiFields;
-import org.apache.lucene.index.PostingsEnum;
-import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.ConstantScoreWeight;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.search.TopScoreDocCollector;
-import org.apache.lucene.search.Weight;
-import org.apache.lucene.search.similarities.DFISimilarity;
+import org.apache.lucene.search.similarities.LMDirichletSimilarity;
+import org.apache.lucene.search.similarities.LMJelinekMercerSimilarity;
 import org.apache.lucene.search.similarities.Similarity;
-import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.BytesRef;
-
-import jdk.nashorn.internal.runtime.regexp.joni.Matcher;
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
 public class compareAlgorithms {
 	public class storequery{
 		String title;
@@ -98,7 +74,7 @@ public class compareAlgorithms {
 			TopDocs results = searcher.search(query, 1000);		
 			//Print number of hits
 			int numTotalHits = results.totalHits;
-			System.out.println(numTotalHits + " total matching documents");
+			//System.out.println(numTotalHits + " total matching documents");
 			//Print retrieved results
 			int count=0;
 			ScoreDoc[] hits = results.scoreDocs;
@@ -112,6 +88,7 @@ public class compareAlgorithms {
 		}
 		fwriter.close();
 		reader.close();
+		System.out.println("DONE");
 	}
 	public static void main(String args[]) throws IOException, ParseException{
 		compareAlgorithms c=new compareAlgorithms();
@@ -123,7 +100,19 @@ public class compareAlgorithms {
 			o.get(i).title=o.get(i).title.trim();
 			//System.out.println(o.get(i).qid+"\n"+o.get(i).title+"\n"+o.get(i).desc);
 		}
+		/*Short Query*/
 		Similarity similarity=new BM25Similarity();
 		c.compareAlgos("C:\\Users\\sujit\\Desktop\\Search\\Assignment 2\\BM25longshortQuery.txt",similarity,o);
+		
+		//Default Similarity has deprecated, using Classic Similarity Instead.
+		similarity=new ClassicSimilarity();
+		c.compareAlgos("C:\\Users\\sujit\\Desktop\\Search\\Assignment 2\\ClassicSimilarityshortQuery.txt",similarity,o);
+
+		similarity=new LMDirichletSimilarity();
+		c.compareAlgos("C:\\Users\\sujit\\Desktop\\Search\\Assignment 2\\LMDirichletSimilarityshortQuery.txt",similarity,o);
+		
+		similarity=new LMJelinekMercerSimilarity((float) 0.7);
+		c.compareAlgos("C:\\Users\\sujit\\Desktop\\Search\\Assignment 2\\LMJelinekMercerSimilarityshortQuery.txt",similarity,o);
+		
 	}
 }
